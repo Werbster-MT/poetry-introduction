5414490545c1f5bdc5259bf410c37aedcb6447d2
Werbster <wmarques.88@hotmail.com>
2022-05-08 00:00:21 -0300

    test

--- tests/test_poetry_introduction.py
+++ tests/test_poetry_introduction.py
@@ -2,4 +2,14 @@ from poetry_introduction import __version__
 
 
 def test_version():
-    assert __version__ == '0.1.0'
+    assert __version__ == "0.1.0"
+
+
+def very_long_function(
+    long_variable_name,
+    long_variable_name2,
+    long_variable_name3,
+    long_variable_name4,
+    long_variable_name5,
+):
+    pass
